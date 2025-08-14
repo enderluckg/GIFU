2ad88f164eba2926a1a8dd1c7d90665c57020f53
LUCAS_SALMAN_RESENDE <12401080@ALFAB35.BARROCA>
2025-08-14 07:46:21 -0300

    first commit

---
+++ global.js
@@ -0,0 +1,20 @@
+
+let mario = {nome:"Super Mario 3D Word", quantidade:0 , preco:350 }
+
+function btnmario(){
+
+let divPrincipal = document.createElement("div");
+
+divPrincipal.classList.add("jogo");
+
+let div1 = document.createElement("div");
+div1.classList.add("org4")
+
+let div2 = document.createElement("div");
+div2.classList.add("org5")
+
+
+divPrincipal.appendChild(div1)
+
+ 
+}
---
+++ home.js
@@ -0,0 +1,17 @@
+const video = document.getElementById('videoclipes');
+
+window.addEventListener('scroll', () => {
+  const scrollY = window.scrollY;
+
+  if (scrollY > 95) {
+    video.style.width = '90%';
+    video.style.height = 'auto';
+    video.style.borderRadius = "100px"
+    video.style.marginTop = "15vh"
+  }
+  else{
+    video.style.width = '100%';
+    video.style.borderRadius ="0px"
+    video.style.marginTop = "0vh"
+  }
+});
